59c6d14ca79e950174e21fb58fa422cb10ece075
Jacob Manning <jacobmanning@pitt.edu>
2018-07-06 22:01:08 -0400

    Add OpenCV boilerplate

---
+++ opencv/src/DisplayImage.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main(int argc, const char** argv) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " </path/to/image>" << std::endl;
+        return 1;
+    }
+
+    auto image = cv::imread(argv[1], 1);
+
+    if (!image.data) {
+        std::cerr << "No image data!" << std::endl;
+        return 1;
+    }
+
+    cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Display Image", image);
+    cv::waitKey(0);
+
+    return 0;
+}
