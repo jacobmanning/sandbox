16f40f32017de2d5737699ff1f43ca898e4cc852
Jacob Manning <jacobmanning@pitt.edu>
2019-02-21 18:54:40 -0500

    Use structured bindings for tuples

--- cpp17/src/tuples.cc
+++ cpp17/src/tuples.cc
@@ -9,13 +9,13 @@
 
 int main(int argc, const char** argv) {
     // Create a tuple
-    auto my_tuple = std::make_tuple(std::string("Jacob"), 20, 0);
+    auto my_tuple = std::make_tuple(std::string{"Jacob"}, 20, 0);
     // Print original values
     util::print_tuple(my_tuple);
 
     // Access members by value
-    std::string name = std::get<0>(my_tuple);
-    int age = std::get<1>(my_tuple);
+    auto name = std::get<0>(my_tuple);
+    auto age = std::get<1>(my_tuple);
 
     // Attempt to change members by copy
     name = "Roger";
@@ -36,14 +36,25 @@ int main(int argc, const char** argv) {
     util::print_tuple(my_tuple);
 
     // Or can get reference and modify that reference
-    std::string& name_ref = std::get<0>(my_tuple);
-    int& age_ref = std::get<1>(my_tuple);
-    int& num_ref = std::get<2>(my_tuple);
+    auto& name_ref = std::get<0>(my_tuple);
+    auto& age_ref = std::get<1>(my_tuple);
+    auto& num_ref = std::get<2>(my_tuple);
 
     // Modify values in tuple with reference
     name_ref = "Jacob!";
     age_ref = 200;
     num_ref = 4;
+
+    // Print to show that values have changed again
+    util::print_tuple(my_tuple);
+
+    // Can also use C++17 structured bindings
+    auto& [name_ref_v2, age_ref_v2, num_ref_v2] = my_tuple;
+
+    // Modify by ref again
+    name_ref_v2 = "Jaaaaacob";
+    age_ref_v2 = 1337;
+    num_ref_v2 = 7;
 
     // Print to show that values have changed again
     util::print_tuple(my_tuple);
