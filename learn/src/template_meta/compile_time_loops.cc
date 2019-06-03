574b49fc0b2b7180d5756dd414b8729b9617b8b0
Jacob Manning <jacobmanning@pitt.edu>
2019-06-03 09:44:28 -0400

    Fix compile time loops example to actually work

--- learn/src/template_meta/compile_time_loops.cc
+++ learn/src/template_meta/compile_time_loops.cc
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <string>
+#include <utility>
 
 namespace ClassMethodLooper {
 
 template <int N,
-          template <int N, typename...> class Wrapper,
+          template <int M, typename...> class Wrapper,
           typename Parent,
           typename... Args>
 struct Loop {
@@ -16,18 +18,17 @@ struct Loop {
   }
 };
 
-template <int N,
-          template <int N, typename...> class Wrapper,
+template <template <int M, typename...> class Wrapper,
           typename Parent,
           typename... Args>
 struct Loop<0, Wrapper, Parent, Args...> {
-  static void impl(Parent* parent, Args&&... args) {}
+  static void impl([[maybe_unused]] Parent* parent, [[maybe_unused]] Args&&... args) {}
 };
 
 } // namespace ClassMethodLooper
 
 template <int N,
-          template <int N, typename...> class Wrapper,
+          template <int M, typename...> class Wrapper,
           typename Parent,
           typename... Args>
 void ClassMethodLoop(Parent* parent, Args&&... args) {
@@ -36,14 +37,14 @@ void ClassMethodLoop(Parent* parent, Args&&... args) {
 
 class HouseCleaner {
 public:
-  void clean_all(const std::string& tool) {
-    ClassMethodLoop<4, CleanHouseWrapper>(this, tool);
+  void clean_all(std::string&& tool) {
+    ClassMethodLoop<4, CleanHouseWrapper>(this, std::forward<std::string>(tool));
   }
 
 private:
-  std::string message_{"Cleaning house #"};
+  const std::string message_{"Cleaning house #"};
 
-  template <int N>
+  template <int N, typename... Args>
   struct CleanHouseWrapper {
     void operator()(HouseCleaner* parent, std::string&& tool) const {
       parent->clean_house<N>(std::forward<std::string>(tool));
@@ -51,14 +52,12 @@ private:
   };
 
   template <int N>
-  void clean_house(const std::string& tool) {
+  void clean_house(const std::string& tool) const {
     std::cout << message_ << N << " with " << tool << "\n";
   }
 };
 
 int main() {
-  // auto cleaner = std::make_unique<HouseCleaner>();
-  // cleaner->clean_all("broom");
   auto cleaner = HouseCleaner();
   cleaner.clean_all("broom");
 }
