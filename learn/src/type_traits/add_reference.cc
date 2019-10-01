a4ce43ec0bb54e3460cf53e23ab9eb70a122e646
Jacob Manning <jacobmanning@pitt.edu>
2019-09-30 22:38:49 -0400

    Add some basic type traits usage

---
+++ learn/src/type_traits/add_reference.cc
@@ -0,0 +1,28 @@
+#include <array>
+#include <string>
+#include <type_traits>
+#include <vector>
+
+namespace
+{
+
+template <typename T>
+void assert_valid_add_reference()
+{
+  static_assert(std::is_same_v<T&, std::add_lvalue_reference_t<T>>);
+  static_assert(std::is_same_v<T&&, std::add_rvalue_reference_t<T>>);
+}
+
+}  // namespace
+
+int main()
+{
+  assert_valid_add_reference<int>();
+  assert_valid_add_reference<float>();
+  assert_valid_add_reference<double>();
+
+  assert_valid_add_reference<std::string>();
+
+  assert_valid_add_reference<std::array<int, 4>>();
+  assert_valid_add_reference<std::vector<int>>();
+}
---
+++ learn/src/type_traits/remove_reference.cc
@@ -0,0 +1,29 @@
+#include <array>
+#include <string>
+#include <type_traits>
+#include <vector>
+
+namespace
+{
+
+template <typename T>
+void assert_valid_remove_reference()
+{
+  static_assert(std::is_same_v<T, std::remove_reference_t<T>>);
+  static_assert(std::is_same_v<T, std::remove_reference_t<T&>>);
+  static_assert(std::is_same_v<T, std::remove_reference_t<T&&>>);
+}
+
+}  // namespace
+
+int main()
+{
+  assert_valid_remove_reference<int>();
+  assert_valid_remove_reference<float>();
+  assert_valid_remove_reference<double>();
+
+  assert_valid_remove_reference<std::string>();
+
+  assert_valid_remove_reference<std::array<int, 4>>();
+  assert_valid_remove_reference<std::vector<int>>();
+}
