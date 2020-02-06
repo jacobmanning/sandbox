3bc5bc7941c05bf590b3f6366d389158952adacb
Jacob Manning <jacobmanning@pitt.edu>
2020-02-05 21:40:27 -0500

    Print tuples properly

--- cpp17/src/util.h
+++ cpp17/src/util.h
@@ -3,31 +3,39 @@
 #include <algorithm>
 #include <iostream>
 #include <tuple>
+#include <utility>
 #include <vector>
 
-namespace util {
-
-/**
- * Helper function to print a vector
- */
+namespace util
+{
+// Helper function to print a vector
 template <class T>
-void print_vector(const std::vector<T>& v) {
-    // Print all elements
-    std::for_each(std::begin(v), std::end(v),
-                  [](const T& n) { std::cout << n << " "; });
+void print_vector(const std::vector<T>& v)
+{
+  // Print all elements
+  std::for_each(std::begin(v), std::end(v), [](const T& n) { std::cout << n << " "; });
 
-    // Print separator
-    std::cout << "\n\n";
+  // Print separator
+  std::cout << "\n\n";
 }
 
-/**
- * Helper function to print the simple tuple example used here
- */
-template <class T, class U, class V>
-void print_tuple(const std::tuple<T, U, V>& t) {
-    std::cout << "First: " << std::get<0>(t) << ". "
-              << "Second: " << std::get<1>(t) << ". "
-              << "Third: " << std::get<2>(t) << '\n';
+namespace impl
+{
+// Variadic print_tuple implementation
+template <class Tuple, std::size_t... Indices>
+void print_tuple(const Tuple& t, std::ostream& os, std::index_sequence<Indices...>)
+{
+  ((os << std::get<Indices>(t) << " "), ...);
+  os << '\n';
 }
 
-}  // namespace util
+} // namespace impl
+
+// Helper function to print the simple tuple example used here
+template <template <class...> class TupleLike, class... Args>
+void print_tuple(const TupleLike<Args...>& t, std::ostream& os = std::cout)
+{
+  impl::print_tuple(t, os, std::index_sequence_for<Args...>{});
+}
+
+} // namespace util
